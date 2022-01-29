b718bc0a6017e9f41a4301fdf32a4264f2cf6dea
PwntasticKev <pwntastickevin@gmail.com>
2022-01-29 12:07:26 -0700

    adding some prisma stuff

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const { PrismaClient } = require("@prisma/client");
+const dateTime = require("node-datetime");
 
 const prisma = new PrismaClient();
 
@@ -12,35 +14,41 @@ app.use(bodyParser.json());
 app.use("/", express.static("./public"));
 
 app.get("/test", async function (req, res) {
+  const date = dateTime.create().format("Y-m-d H:M:S");
+  console.log("yee", typeof date);
   const allQues = await prisma.que.findMany();
   res.status(200).send(allQues);
 });
 
 app.put("/waitlist", async (req, res) => {
-  console.log("REQ BODY BABY", req.body);
   const { email, name, id } = req.body;
   const que = await prisma.que
     .findMany({
       where: {
-        id: req.body.id,
+        id,
       },
       take: 1,
     })
     .then((_) => _[0]);
 
-  if (que) {
-    console.log("after", que);
-    await prisma.que.update({
-      where: {
-        id,
-      },
-      data: {
-        waiting_list: que.waiting_list.push({ name, email }),
-      },
-    });
+  const date = dateTime.create().format("Y-m-d H:M:S");
 
-    if (res.status == 200) return res.sendStatus(200).send("");
-  }
+  console.log(date);
+  await prisma.que.update({
+    data: {
+      waiting_list: JSON.stringify([
+        ...JSON.parse(que.waiting_list),
+        { name, email, claimTime: date },
+      ]),
+      status: "Claimed",
+      claimed_by: que.name,
+    },
+    where: {
+      id: que.id,
+    },
+  });
+
+  if (res.status == 200) return res.sendStatus(200).send();
 });
 
 app.listen(5000, () => {
